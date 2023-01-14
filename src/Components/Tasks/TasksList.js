ce668105464a7b495b531103e5b9691050c15a67
Miloš Nalović <milos.nalovic@gmail.com>
2023-01-14 13:53:03 +0100

    added css for tasks lists

--- src/Components/Tasks/TasksList.js
+++ src/Components/Tasks/TasksList.js
@@ -1,7 +1,8 @@
 import Wrapper from "../UI/Wrapper";
+import styles from "./TasksList.module.css";
 const TaskList = (props) => {
   return (
-    <Wrapper>
+    <Wrapper className={styles.tasks}>
       <ul>
         {props.tasks.map((task) => (
           <li key={task.id}>{task.name} </li>
